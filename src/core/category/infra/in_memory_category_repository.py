90a6c4f162bb2a8f5f57bb926199c83c81eb3613
jbonfim <jabesbonfim@gmail.com>
2024-12-05 12:18:20 -0300

    add method delete

--- src/core/category/infra/in_memory_category_repository.py
+++ src/core/category/infra/in_memory_category_repository.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+from core.category.domain.category import Category
 from src.core.category.application.category_repository import CategoryRepository
 
 
@@ -7,3 +9,13 @@ class InMemoryCategoryRepository(CategoryRepository):
 
     def save(self, category):
         self.categories.append(category)
+        
+    def get_by_id(self, id: UUID) -> Category | None:
+        return next(
+            (category for category in self.categories if category.id == id), None
+        )
+
+    def delete(self, id: UUID) -> None:
+        category = self.get_by_id(id)
+        if category:
+            self.categories.remove(category)

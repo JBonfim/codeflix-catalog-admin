045d21a7dedd9d941cdda5268e20cad60bd65123
jbonfim <jabesbonfim@gmail.com>
2024-12-05 12:27:51 -0300

    add method get

---
+++ src/core/category/tests/application/use_cases/integration/test_get_category.py
@@ -0,0 +1,67 @@
+
+import uuid
+
+import pytest
+from core.category.application.user_cases.exceptions import CategoryNotFound
+from core.category.application.user_cases.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
+from core.category.domain.category import Category
+from core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+
+class TestGetCategory:
+    def test_get_category(self):
+        category_filme = Category(
+            id=uuid.uuid4(),
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True,
+        )
+        category_serie = Category(
+            id=uuid.uuid4(),
+            name="Série",
+            description="Categoria para séries",
+            is_active=True,
+        )
+        repository = InMemoryCategoryRepository(
+            categories=[
+                category_filme,
+                category_serie,
+            ]
+        )
+
+        use_case = GetCategory(repository=repository)
+        request = GetCategoryRequest(id=category_filme.id)
+        response = use_case.execute(request)
+
+        assert response == GetCategoryResponse(
+            id=category_filme.id,
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True,
+        )
+
+    def test_when_category_with_id_does_not_exist_then_raise_not_found(self):
+        category_filme = Category(
+            id=uuid.uuid4(),
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True,
+        )
+        category_serie = Category(
+            id=uuid.uuid4(),
+            name="Série",
+            description="Categoria para séries",
+            is_active=True,
+        )
+        repository = InMemoryCategoryRepository(
+            categories=[
+                category_filme,
+                category_serie,
+            ]
+        )
+
+        use_case = GetCategory(repository=repository)
+        request = GetCategoryRequest(id="non-existent-id")
+
+        with pytest.raises(CategoryNotFound) as exc:
+            use_case.execute(request)
